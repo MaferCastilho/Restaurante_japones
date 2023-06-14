bd6a905777d18417d8c8dffd0832270b33988d64
Maria Fernanda Castilho <castilhomariafer13@gmail.com>
2023-06-14 07:12:55 -0300

    organiza arq. de programacao de pedidos.

---
+++ index.js
@@ -0,0 +1,23 @@
+/*Códigos para funcionar botões e img*/
+
+function AdicionarPedidos(){
+    location.replace("index.html", "_self");
+}
+function FinalizarPedido(){
+        location.replace("finalizar.html", "_self");
+}
+function PagarAgora(){
+    location.replace("qrcode.html", "_self");
+}
+function PagarnoCaixa(){
+    location.replace("caixa.html", "_self");
+}
+function TelaInicial(){
+    location.replace("index.html", "_self");
+}
+function RodíziodaCasa(){
+    location.replace("casa.html","_self");
+}
+function RodízioPersonalizado(){
+    location.replace("personalizado.html","_self");
+}
